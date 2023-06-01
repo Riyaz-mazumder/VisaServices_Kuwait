636449d44b234736b2340b0e37e1b417df97ea59
Riyaz <riajmazumder711@gmail.ocm>
2023-06-01 18:54:23 +0600

    added some more style and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './common/landing-page/landing-page.component';
+import { HomePageComponent } from './frontEnd/home-page/home-page.component';
+import { AboutUsComponent } from './frontEnd/about-us/about-us.component';
+import { KuwaitVisaServicesComponent } from './frontEnd/kuwait-visa-services/kuwait-visa-services.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'aboutUs', component: AboutUsComponent },
+  { path: 'kuwaitVisaServices', component: KuwaitVisaServicesComponent},
   
 ];
 
